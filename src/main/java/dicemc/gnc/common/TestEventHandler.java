ccafe265c4313d737d48748bf4dec7950e34d097
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-07-12 15:41:18 -0400

    Test of chunk event

--- src/main/java/dicemc/gnc/common/TestEventHandler.java
+++ src/main/java/dicemc/gnc/common/TestEventHandler.java
@@ -1,6 +1,9 @@
 package dicemc.gnc.common;
 
 import dicemc.gnc.command.impl.DBCommands;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.event.entity.EntityEvent.EnteringChunk;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
@@ -13,6 +16,19 @@ public class TestEventHandler {
 	public static void onServerStart(FMLServerStartingEvent event) {
 		DBCommands.register(event.getCommandDispatcher());
 		dbConn = new MySQLCon();
+	}
+	
+	@SubscribeEvent
+	public static void onChunkEnterEvent(EnteringChunk event) {
+		if (event.getEntity() instanceof PlayerEntity) {
+			int oldX = event.getOldChunkX();
+			int oldZ = event.getOldChunkZ();
+			int newX = event.getNewChunkX();
+			int newZ = event.getNewChunkZ();
+			if (oldX != newX || oldZ != newZ) {
+				event.getEntity().sendMessage(new StringTextComponent("Entering: ("+String.valueOf(newX)+", "+String.valueOf(newZ)+")."), event.getEntity().getUniqueID());
+			}
+		}
 	}
 
 }

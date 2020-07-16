b6d9131defa35d2dff07cf9b0c064b5f0edc0561
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-07-16 18:58:56 -0400

    removed logger for lack of use.

--- src/main/java/dicemc/gnc/GnC.java
+++ src/main/java/dicemc/gnc/GnC.java
@@ -5,23 +5,19 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import dicemc.gnc.database.DatabaseManager;
 import dicemc.gnc.setup.ClientSetup;
 import dicemc.gnc.setup.CommonSetup;
-import dicemc.gnc.testmaterial.Config;
+import dicemc.gnc.setup.Config;
 
 @Mod(GnC.MOD_ID)
 public class GnC {
 	public static final String MOD_ID = "gnc";
-	private static final Logger LOGGER = LogManager.getLogger();
+	public static DatabaseManager DBM_MAIN, DBM_ALT;
 	
 	public GnC() {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_CONFIG);
-		LOGGER.info("Config Registered");
 		
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(CommonSetup::init);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientSetup::init);

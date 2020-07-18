a8444edae76598b3e29475e7beb46a591330845b
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-07-18 09:50:49 -0400

    added base components

--- src/main/java/dicemc/gnc/guild/Guild.java
+++ src/main/java/dicemc/gnc/guild/Guild.java
@@ -1,6 +1,33 @@
 package dicemc.gnc.guild;
 
-public class Guild {
+import java.sql.ResultSet;
+import java.util.UUID;
 
-	public Guild(String name) {}
+import net.minecraft.nbt.CompoundNBT;
+
+public class Guild {
+	public int GuildID;
+	public String Name;
+	public boolean Open;
+	public double Tax;
+	public Map<Integer, String> PermLevels = new HashMap<Integer, String>();
+	public Map<String, Integer> Permissions = new HashMap<String, Integer>();
+	public Map<UUID, Integer> Members = new HashMap<UUID, Integer>();
+	
+
+	public Guild(String name) {
+		GuildID = -1;
+		Name = name;
+		Open = false;
+		Tax = 0.0;
+	}
+	
+	public Guild(ResultSet rs) {}
+	
+	public Guild(CompoundNBT nbt) {}
+	
+	public CompoundNBT toNBT() {
+		CompoundNBT nbt = new CompoundNBT();		
+		return nbt;
+	}
 }

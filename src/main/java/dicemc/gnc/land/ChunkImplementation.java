55ce82fa761ae75b4c5455e97830f483a797bee0
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-07-18 22:04:30 -0400

    Update and rename ChunkImplementation.java to ChunkManager.java

--- src/main/java/dicemc/gnc/land/ChunkImplementation.java
+++
@@ -1,5 +0,0 @@
-package dicemc.gnc.land;
-
-public class ChunkImplementation {
-
-}
---
+++ src/main/java/dicemc/gnc/land/ChunkManager.java
@@ -0,0 +1,29 @@
+package dicemc.gnc.land;
+
+public class ChunkManager {
+	private Map<ChunkPos, ChunkData> cap = HashMap<ChunkPos, ChunkData>();
+
+	public ChunkManager() {}
+	
+	public String addChunk(ChunkPos) {return "";}
+	
+	public String updateChunk(ChunkPos ck, Map<String, String> values) {return "";}
+	
+	public ChunkData getChunk(ChunkPos) {return new ChunkData();}
+	
+	public String setWhitelist(ChunkPos ck, List<WhitelistItem> whitelist) {return "";}
+	
+	public String updateWhitelistItem(ChunkPos ck, WhitelistItem wlItem) {return "";}
+	
+	public String removeWhitelistItem(ChunkPos ck, String item) {return "";}
+	
+	public List<WhitelistItem> getWhitelist(ChunkPos ck) {return new ArrayList<WhitelistItem>();}
+	
+	public String addPlayer(ChunkPos ck, UUID player, boolean principal) {return "";}
+	
+	public String updatePlayer(ChunkPos ck, UUID player, boolean principal) {return "";}
+	
+	public String removePlayer(ChunkPos ck, UUID player) {return "";}
+	
+	public Map<UUID, String> getPlayers(ChunkPos ck) {return new HashMap<UUID, String>();}
+}

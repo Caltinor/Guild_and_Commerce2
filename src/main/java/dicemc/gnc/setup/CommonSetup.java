5f6bd7ac198380b50ce26a97fe8838f16e508fb0
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-07-28 18:01:35 -0400

    Removed instantiating logic.

--- src/main/java/dicemc/gnc/setup/CommonSetup.java
+++ src/main/java/dicemc/gnc/setup/CommonSetup.java
@@ -2,8 +2,6 @@ package dicemc.gnc.setup;
 
 import dicemc.gnc.GnC;
 import dicemc.gnc.datastorage.database.DatabaseManager;
-import dicemc.gnc.datastorage.wsd.MarketWSD;
-import dicemc.gnc.datastorage.wsd.WorldWSD;
 import dicemc.gnc.land.ChunkManager;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,13 +13,12 @@ public class CommonSetup {
 	public static void init(final FMLCommonSetupEvent event) {
 		Networking.registerMessages();
 		GnC.ckMgr = new ChunkManager();
+		if (Config.WORLD_USE_DB.get())  GnC.DBM_MAIN = new DatabaseManager(true);
+		if (Config.MARKET_USE_DB.get()) GnC.DBM_ALT = new DatabaseManager(false);
 	}
 	
 	@SubscribeEvent
 	public static void onServerStart(FMLServerStartingEvent event) {
-		if (Config.WORLD_USE_DB.get())  GnC.DBM_MAIN = new DatabaseManager(true);
-		else GnC.worldSaver = new WorldWSD();
-		if (Config.MARKET_USE_DB.get()) GnC.DBM_ALT = new DatabaseManager(false);
-		else GnC.marketSaver = new MarketWSD();
+		
 	}
 }

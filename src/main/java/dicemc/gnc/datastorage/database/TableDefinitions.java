2d46c10444d24391a8e1cee2b1c3344733efadc2
Caltinor <62700786+Caltinor@users.noreply.github.com>
2020-08-02 17:56:31 -0400

    Finalize definitions

--- src/main/java/dicemc/gnc/datastorage/database/TableDefinitions.java
+++ src/main/java/dicemc/gnc/datastorage/database/TableDefinitions.java
@@ -12,19 +12,49 @@ public class TableDefinitions {
 	
 	private Map<String, String> defineTables(String suffix) {
 		Map<String, String> map = new HashMap<String, String>();
-		//String sqlSTR = "";
-		//String tblSTR = "";
-		//Guild Table
-		/*tblSTR = map_Guild.get(tblGuild.TABLE_NAME) + suffix;
-		sqlSTR = " ("+ map_Guild.get(tblGuild.GUILD_ID)+" INT NOT NULL AUTO_INCREMENT, " + 
-				map_Guild.get(tblGuild.NAME) +" VARCHAR(32) NOT NULL, " + 
-				map_Guild.get(tblGuild.OPEN) +" TINYINT(1) NOT NULL, " + 
-				map_Guild.get(tblGuild.TAX) +" DOUBLE NOT NULL, " + 
-				"PRIMARY KEY ("+ map_Guild.get(tblGuild.GUILD_ID) +"))";
+		String sqlSTR = "";
+		String tblSTR = "";
+		//Account Table
+		tblSTR = map_Accounts.get(tblAccounts.TABLE_NAME) + suffix;
+		sqlSTR = " ("+ map_Accounts.get(tblAccounts.ID)	+" INT NOT NULL AUTO_INCREMENT, " + 
+				map_Accounts.get(tblAccounts.OWNER) 	+" VARCHAR(36) NOT NULL, " + 
+				map_Accounts.get(tblAccounts.BALANCE) 	+" DOUBLE NOT NULL, " + 
+				"PRIMARY KEY ("+ map_Accounts.get(tblAccounts.ID) +"))";
 		map.put(tblSTR, sqlSTR);
-		*/
-		//repeat
-		//TODO: Build Table Creation Definitions
+		//Market Table
+		tblSTR = map_Markets.get(tblMarkets.TABLE_NAME) + suffix;
+		sqlSTR = " ("+ map_Markets.get(tblMarkets.ID) 			+" INT NOT NULL AUTO_INCREMENT, " +
+				map_Markets.get(tblMarkets.MARKET_TYPE) 		+" INT NOT NULL, " +
+				map_Markets.get(tblMarkets.ITEM) 				+" LONGTEXT NOT NULL, " +
+				map_Markets.get(tblMarkets.VENDOR_ID)			+" VARCHAR(36) NOT NULL, " +
+				map_Markets.get(tblMarkets.LOCALITY)			+" VARCHAR(36) NOT NULL, " +
+				map_Markets.get(tblMarkets.BID_END)				+" MEDIUMTEXT NOT NULL, " +
+				map_Markets.get(tblMarkets.PRICE)				+" DOUBLE NOT NULL, " +
+				map_Markets.get(tblMarkets.VENDOR_GIVE_ITEM)	+" TINYINT(1) NOT NULL, " +
+				map_Markets.get(tblMarkets.STOCK)				+" INT NOT NULL, " +
+				map_Markets.get(tblMarkets.ACTIVE_TRANSACTION)	+" TINYINT(1) NOT NULL, " +
+				map_Markets.get(tblMarkets.BUYER_ID)			+" VARCHAR(36) NOT NULL, " +
+				map_Markets.get(tblMarkets.DTG_PLACED)			+" MEDIUMTEXT NOT NULL, " +
+				map_Markets.get(tblMarkets.DTG_BOUGHT)			+" MEDIUMTEXT NOT NULL, " +
+				"PRIMARY KEY (" + map_Markets.get(tblMarkets.ID) + "))";
+		map.put(tblSTR, sqlSTR);
+		//Bids Table
+		tblSTR = map_Bids.get(tblBids.TABLE_NAME) + suffix;
+		sqlSTR = " ("+ map_Bids.get(tblBids.ID) 		+" INT NOT NULL AUTO_INCREMENT, " +
+				map_Bids.get(tblBids.TRANSACTION_ID) 	+" INT NOT NULL, " +
+				map_Bids.get(tblBids.BIDDER_ID) 		+" VARCHAR(36) NOT NULL, " +
+				map_Bids.get(tblBids.DTG_PLACED) 		+" MEDIUMTEXT NOT NULL, " +
+				map_Bids.get(tblBids.PRICE)				+" DOUBLE NOT NULL, " +
+				"PRIMARY KEY (" + map_Bids.get(tblBids.ID) + "))";
+		map.put(tblSTR, sqlSTR);
+		//Storage Table
+		tblSTR = map_Storage.get(tblStorage.TABLE_NAME) + suffix;
+		sqlSTR = " ("+ map_Storage.get(tblStorage.ID) 	+" INT NOT NULL AUTO_INCREMENT, " +
+				map_Storage.get(tblStorage.OWNER) 		+" VARCHAR(36) NOT NULL, " +
+				map_Storage.get(tblStorage.ITEM) 		+" MEDIUMTEXT NOT NULL, " +
+				map_Storage.get(tblStorage.QUANTITY) 	+" INT NOT NULL, " +
+				"PRIMARY KEY (" + map_Storage.get(tblStorage.ID) + "))";
+		map.put(tblSTR, sqlSTR);
 		return map;
 	}
 	
@@ -41,7 +71,7 @@ public class TableDefinitions {
 	
 	//INFINITE replaced with using negative values for stock
 	public static enum tblMarkets {TABLE_NAME, ID, MARKET_TYPE, ITEM, VENDOR_ID, LOCALITY, 
-		BID_END, PRICE, VENDOR_GIVE_ITEM, STOCK, ACTIVE_TRANSACTION, BUYER_ID}
+		BID_END, PRICE, VENDOR_GIVE_ITEM, STOCK, ACTIVE_TRANSACTION, BUYER_ID, DTG_PLACED, DTG_BOUGHT}
 	public static final Map<tblMarkets, String> map_Markets = define_Markets();
 	private static Map<tblMarkets, String> define_Markets() {
 		Map<tblMarkets, String> map = new HashMap<tblMarkets, String>();
@@ -57,6 +87,8 @@ public class TableDefinitions {
 		map.put(tblMarkets.STOCK, "Stock");
 		map.put(tblMarkets.ACTIVE_TRANSACTION, "active");
 		map.put(tblMarkets.BUYER_ID, "buyer");
+		map.put(tblMarkets.DTG_PLACED, "dtg_placed");
+		map.put(tblMarkets.DTG_BOUGHT, "dtg_bought");
 		return map;
 	}
 	

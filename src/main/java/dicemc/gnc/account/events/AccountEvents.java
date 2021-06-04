23971f53172c239cb0d2b90fb306a97a7dbe6452
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-06-03 20:18:51 -0400

    update to use Lib account behaviors

--- src/main/java/dicemc/gnc/account/events/AccountEvents.java
+++ src/main/java/dicemc/gnc/account/events/AccountEvents.java
@@ -1,18 +1,25 @@
 package dicemc.gnc.account.events;
 
-import dicemc.gnc.GnC;
+import java.util.logging.Logger;
+
+import dicemc.gnclib.money.LogicMoney;
+import dicemc.gnclib.money.LogicMoney.AccountType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class AccountEvents {
+	private static Logger LOG = Logger.getLogger("dicemc.gnc.account.events");
 
 	@SubscribeEvent
 	public static void onPlayerLogin(PlayerLoggedInEvent event) {
 		if (event.getEntity() instanceof PlayerEntity) {
-			System.out.println(GnC.aMgr.accountExists(event.getEntity().getUniqueID(), true));
+			Double bal = LogicMoney.getBalance(event.getEntity().getUUID(), AccountType.PLAYER.rl);
+			LOG.info("Player: "+event.getPlayer().getDisplayName().getString() + " joined with $"+bal);
+			event.getPlayer().sendMessage(new TranslationTextComponent("gnc.money.events.login", bal), event.getPlayer().getUUID());
 		}
 	}
 }

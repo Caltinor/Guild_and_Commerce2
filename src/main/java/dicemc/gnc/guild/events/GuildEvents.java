2ec9fbdb4ed478107f54595e80913ad64cb7ce71
Caltinor <62700786+Caltinor@users.noreply.github.com>
2021-06-03 20:24:46 -0400

    updated taxation method

--- src/main/java/dicemc/gnc/guild/events/GuildEvents.java
+++ src/main/java/dicemc/gnc/guild/events/GuildEvents.java
@@ -1,7 +1,14 @@
 package dicemc.gnc.guild.events;
 
-import dicemc.gnc.GnC;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import dicemc.gnc.land.WorldWSD;
 import dicemc.gnc.setup.Config;
+import dicemc.gnclib.guilds.LogicGuilds;
+import dicemc.gnclib.util.Duo;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.ServerTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,14 +17,18 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class GuildEvents {
 	private static int timer = 0;
+	public static Iterable<ServerWorld> worlds;
 
 	@SubscribeEvent
 	public static void onServerTick(ServerTickEvent event) {
 		if (event.phase == Phase.END) {
 			timer++;
 			if (timer >= Config.GLOBAL_TAX_INTERVAL.get()) {
-				GnC.gMgr.applyTaxes();
-				GnC.gMgr.printTaxInfo();
+				Map<UUID, Duo<Integer, Double>> taxData = new HashMap<UUID, Duo<Integer, Double>>();
+				for (ServerWorld world : worlds) {
+					taxData.putAll(WorldWSD.get(world).getTaxData());
+				}
+				LogicGuilds.applyTaxes(taxData);
 			}
 		}
 	}
